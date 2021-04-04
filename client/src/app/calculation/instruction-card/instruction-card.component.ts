40f07033035afac9fce72159eb0f13d1fb81aaf6
Tyler Mizuyabu <tylermizuyabu@gmail.com>
2021-04-03 22:17:59 -0400

    fixed typo

--- client/src/app/calculation/instruction-card/instruction-card.component.ts
+++ client/src/app/calculation/instruction-card/instruction-card.component.ts
@@ -17,11 +17,11 @@ export class InstructionCardComponent implements OnInit {
     [Operators.PowerOf, 'Exponent Operator'],
   ];
   static readonly functionDescriptions: [string, string][] = [
-    [minFn.positiveIdentifier, 'max function returns the minimum of two numbers/expressions. Requires exactly 2 arguments'],
+    [minFn.positiveIdentifier, 'min function returns the minimum of two numbers/expressions. Requires exactly 2 arguments'],
     [maxFn.positiveIdentifier, 'max function returns the maximum of two numbers/expressions. Requires exactly 2 arguments'],
     [sqrtFn.positiveIdentifier, 'square root function returns the square root of a number/expression'],
     [logFn.positiveIdentifier, 'log function returns the natural logarithm of a number/expression'],
-    [sinFn.positiveIdentifier, 'sin function returns the sine of a number/expression'],
+    [sinFn.positiveIdentifier, 'sine function returns the sine of a number/expression'],
     [cosFn.positiveIdentifier, 'cosine function returns the cos of a number/expression'],
     [tanFn.positiveIdentifier, 'tanjent function returns the tan of a number/expression'],
   ];
